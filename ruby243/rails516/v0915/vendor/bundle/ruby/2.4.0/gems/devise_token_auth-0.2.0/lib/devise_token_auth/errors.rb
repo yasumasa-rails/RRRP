97d1e93e71dd65b62684ce02e988b550ba45db1d
yasumasa_kataoka <yasuamsa.kataoka@gmail.com>
2019-04-05 15:37:08 +0900

    Signed-off-by: yasumasa

--- ruby243/rails516/v0915/vendor/bundle/ruby/2.4.0/gems/devise_token_auth-0.2.0/lib/devise_token_auth/errors.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module DeviseTokenAuth
-  module Errors
-    class NoResourceDefinedError < StandardError ; end
-  end
-end
--- ruby243/rails516/v0915/vendor/bundle/ruby/2.4.0/gems/ffi-1.9.25/ext/ffi_c/libffi-x86_64-linux/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/mnt/c/ubuntu/RRRp/ruby243/rails516/v0915/vendor/bundle/ruby/2.4.0/gems/ffi-1.9.25/ext/ffi_c/libffi/src/x86/ffitarget.h
